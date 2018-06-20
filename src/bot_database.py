e9457894b6d06e43e684500a4852028aee3b6462
Davit Julakidze <Davit.Julakidze@structuretone.com>
2018-06-20 16:48:14 -0400

    fixing databases function

--- src/bot_database.py
+++ src/bot_database.py
@@ -2,7 +2,7 @@ import sqlite3
 import json
 from datetime import datetime
 
-timeframe = ['2015-05', '2015-04', '2015-03']
+timeframe = '2018-03'
 sql_transaction = []
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
@@ -14,6 +14,29 @@ def create_table():
 def format_data(data):
     data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
     return data
+
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print(str(e))
+        return False
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+    else:
+        return True
+
 
 def find_parent(pid):
     try:
@@ -33,15 +56,28 @@ if __name__ == '__main__':
     row_counter = 0
     paired_rows = 0
 
-    for n in timeframe:
-        with open('F:/reddit_comments/data/RC_{}'.format(n), buffering=1000) as f:
-            for row in f:
-                row_counter += 1
-                row = json.loads(row)
-                parent_id = row['parent_id']
-                body = format_data(row['body'])
-                created_utc = row['created_utc']
-                score = row['score']
-                comment_id = row['name']
-                subreddit = row['subreddit']
-                parent_data = find_parent(parent_id)
+    with open('F:/reddit_comments/data/{}/RC_{}'.format(timeframe.split('-')[0], timeframe),
+              buffering=1000) as f:
+        for row in f:
+            row_counter += 1
+            row = json.loads(row)
+            parent_id = row['parent_id']
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            comment_id = row['link_id']
+            subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+            if score >= 5:
+                if(acceptable(body)):
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if score > existing_comment_score:
+                            sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit, created_utc)
+                    else:
+                        if parent_data:
+                            ####
+                            sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc)
+                        else:
+                            ####
+                            sql_insert_no_parent()
